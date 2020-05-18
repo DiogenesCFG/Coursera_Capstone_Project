4032ca1ce61c304603c0c269be6efb65b8e4b344
DiogenesCFG <diogenescfg@gmail.com>
2020-05-17 21:37:11 -0500

    First app atempt. Runs on computer, but won't deploy on shiny.io Still gotta figure out how to correclty deploy it.

---
+++ PredictionApp.R
@@ -0,0 +1,40 @@
+library(shiny)
+suppressPackageStartupMessages({
+    library(tidyverse)
+    library(stringr)
+})
+
+source("D:\\Users\\dcruz\\Documents\\Coursera_Capstone_Project\\Separated Ngrams\\ngrams.R")
+
+ui <- fluidPage(
+    titlePanel("Text Prediction App"),
+    p("This (rather simple) App predicts the next word from a phrase."), 
+    p("It was developed as a part of the capstone project in the JHU-Coursera Data Cience Specialization"), 
+    
+    sidebarLayout(
+        sidebarPanel(
+            h2("Instructions"),
+            h5("1. Enter a word of a phrase in the text bar at the side"), 
+            h5("2. Below will be shown a list of 3 possible follow up words"), 
+            h5("3. In case of misspelling, probably no only few words will appear")
+        ), 
+        mainPanel(
+            textInput("phraseInput", "User Input", width = 600), 
+            h2("Suggested predicted words"), 
+            h3(span(textOutput("suggested_outputs"), style = "color:blue"))
+        )
+    )
+
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$suggested_outputs <- renderText({
+        predictText(input$phraseInput)
+    })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
